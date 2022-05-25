fb98445d0fe92ee5f9bb92126a2969392df034d9
Supratim Saha <104687502+ssaha1450@users.noreply.github.com>
2022-05-25 15:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (firstLetter === 'j' || firstletter ==='J') {
+    if (firstLetter === 'j' || firstLetter ==='J') {
        byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
